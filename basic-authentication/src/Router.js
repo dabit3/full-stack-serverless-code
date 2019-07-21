35e19c6a268a6e37ff919076448b8c0b7ae80d4e
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-07-21 16:53:32 -0700

    updated route names

--- basic-authentication/src/Router.js
+++ basic-authentication/src/Router.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Nav from './Nav'
-import App from './App'
+import Public from './Public'
 import Profile from './Profile'
 import Protected from './Protected'
 
@@ -13,23 +13,25 @@ const Router = () => {
     const pathname = location[location.length-1]
     setCurrent(pathname ? pathname : 'home')
   }, [])
-  const updateNav = v => {
-    setCurrent(v.key)
-  }
+  const updateNav = value => setCurrent(value.key)
   return (
     <HashRouter>
       <Nav current={current} updateNav={updateNav}  />
       <Switch>
-        <Route exact path="/" component={App}/>
+        <Route exact path="/" component={Public}/>
         <Route
           exact
           path="/protected" 
           render={(routeProps) => (
-            <Protected {...routeProps} current={current} setCurrent={setCurrent} />
+            <Protected
+              {...routeProps}
+              current={current}
+              setCurrent={setCurrent}
+            />
           )}
         />
         <Route exact path="/profile" component={Profile}/>
-        <Route component={App}/>
+        <Route component={Public}/>
       </Switch>
     </HashRouter>
   )
