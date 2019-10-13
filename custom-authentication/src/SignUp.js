dc119a9e7438a4c67e4dc9116531d437542d7328
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-10-12 17:19:58 -0700

    updatetd form to remove prop name

--- custom-authentication/src/SignUp.js
+++ custom-authentication/src/SignUp.js
@@ -2,29 +2,29 @@ import React from 'react'
 import Button from './Button'
 import { styles } from './Form'
 
-function SignUp(props) {
+function SignUp({ updateFormState, signUp }) {
   return (
     <div style={styles.container}>
       <input 
         name='username'
-        onChange={e => {e.persist();props.updateFormState(e)}}
+        onChange={e => {e.persist();updateFormState(e)}}
         style={styles.input}
         placeholder='username'
       />
       <input
         type='password'
         name='password'
-        onChange={e => {e.persist();props.updateFormState(e)}}
+        onChange={e => {e.persist();updateFormState(e)}}
         style={styles.input}
         placeholder='password'
       />
       <input 
         name='email'
-        onChange={e => {e.persist();props.updateFormState(e)}}
+        onChange={e => {e.persist();updateFormState(e)}}
         style={styles.input}
         placeholder='email'
       />
-      <Button onClick={props.signUp} title="Sign Up" />
+      <Button onClick={signUp} title="Sign Up" />
     </div>
   )
 }
