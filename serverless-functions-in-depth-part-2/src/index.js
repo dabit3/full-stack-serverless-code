c29f20d5741a32a3e1db1e2c13afa0508c89782c
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2020-03-17 13:11:49 -0700

    updated imports in index.js

--- serverless-functions-in-depth-part-2/src/index.js
+++ serverless-functions-in-depth-part-2/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Router';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css';
 
@@ -9,7 +9,7 @@ import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
