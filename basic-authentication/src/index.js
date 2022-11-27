bc5945e45617ff4f2b11c75accd54ab4009bd1f4
graysonecooper <graysonecooper@gmail.com>
2022-11-27 15:26:24 -0500

    Amplify named export
    Replaced Amplify with named export
    
    https://github.com/aws-amplify/amplify-js/issues/6411#issuecomment-665719562

--- basic-authentication/src/index.js
+++ basic-authentication/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Router from './Router';
 import 'antd/dist/antd.css';
 
-import Amplify from 'aws-amplify'
+import { Amplify } from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
